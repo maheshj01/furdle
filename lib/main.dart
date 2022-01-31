48d413708952d677afb16ad7cffea754fe506b34
mahesh jamdade <maheshmn121@gmail.com>
2022-01-31 10:27:00 +0530

    add local webview

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,19 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:furdle/pages/home.dart';
+import 'package:furdle/pages/privacy.dart';
 import 'package:furdle/utils/settings_controller.dart';
 import 'package:go_router/go_router.dart';
+import 'package:furdle/utils/register_web_webview_stub.dart'
+    if (dart.library.html) 'package:furdle/utils/register_web.dart';
 import 'constants/constants.dart' show APP_TITLE;
-import 'utils/word.dart';
 
 /// Settings are exposed globally to access from anywhere
 
 late SettingsController settingsController;
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  registerWebViewWebImplementation();
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   GoRouter.setUrlPathStrategy(UrlPathStrategy.path);
@@ -32,6 +35,12 @@ class MyApp extends StatelessWidget {
           title: APP_TITLE,
         ),
       ),
+    ),
+    GoRoute(
+      path: '/privacy',
+      name: 'privacy',
+      pageBuilder: (context, state) =>
+          MaterialPage<void>(key: state.pageKey, child: const Privacy()),
     ),
   ]);
 
---
+++ lib/utils/register_web.dart
@@ -0,0 +1,3 @@
+void registerWebViewWebImplementation() {
+//   WebView.platform = WebWebViewPlatform();
+}
