e0d80439c723712ff8bf8a503bb66e9e3ec64fcc
mahesh jamdade <maheshmn121@gmail.com>
2022-02-02 21:56:20 +0530

    integrate firebase cloud functionns

---
+++ functions/src/index.ts
@@ -0,0 +1,38 @@
+import * as admin from "firebase-admin";
+import * as functions from "firebase-functions";
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+admin.initializeApp(functions.config().firebase);
+
+const wordList:string[] = [
+  "diver",
+  "dizzy",
+  "dodge",
+  "dodgy",
+  "dogma",
+  "doing",
+  "dolly",
+  "donor",
+];
+
+function randomWord(arr:string[]) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+exports.scheduledFunction = functions.pubsub
+    .schedule("0 0 * * *")
+    .timeZone("Asia/Kolkata")
+    .onRun(async () => {
+      const docRef = await admin.firestore().collection("furdle").doc("stats");
+      return await docRef.update({
+        date: new Date(),
+        number: admin.firestore.FieldValue.increment(1),
+        word: randomWord(wordList),
+      });
+    });
