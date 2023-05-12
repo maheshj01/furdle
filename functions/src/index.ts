eec232fe88eebf48876e62e632940d9d119a5680
mahesh jamdade <maheshmn121@gmail.com>
2023-05-11 22:10:44 -0400

    change cron to every 24 hours

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,18 +11,18 @@ import {wordList} from "./word";
 
 admin.initializeApp(functions.config().firebase);
 
-function randomWord(arr:string[]) {
+function randomWord(arr: string[]) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
 // Runs every 6 hours IST
 exports.scheduledFunction = functions.pubsub
-    .schedule("0 */6 * * *")
+    .schedule("0 0 * * *") // midnight every day
     .timeZone("Asia/Kolkata")
     .onRun(async () => {
       const docRef = await admin.firestore().collection("furdle").doc("stats");
       return await docRef.update({
-        date: new Date(),
+        date: admin.firestore.Timestamp.now(),
         number: admin.firestore.FieldValue.increment(1),
         word: randomWord(wordList),
       });
