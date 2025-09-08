4bfa4876446494a8139d8a00b5490a6480935d4e
mahesh jamdade <maheshmn121@gmail.com>
2025-09-07 23:59:04 -0400

    Update firebase function

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -19,13 +19,13 @@ module.exports = {
   },
   ignorePatterns: [
     "/lib/**/*", // Ignore built files.
+    "/generated/**/*", // Ignore generated files.
   ],
-  plugins: [
-    "@typescript-eslint",
-    "import",
-  ],
+  plugins: ["@typescript-eslint", "import"],
   rules: {
     "quotes": ["error", "double"],
-    "require-jsdoc": 0,
+    "import/no-unresolved": 0,
+    // off max line length
+    "max-len": "off",
   },
 };
