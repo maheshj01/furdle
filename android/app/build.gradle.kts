31711bfb8bcdcd656583c8de4b1b03662c984112
mahesh jamdade <maheshmn121@gmail.com>
2026-08-30 13:14:38 -0400

    update target sdk version to 36

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -44,7 +44,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk =  36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
