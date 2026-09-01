6901416ae948b046d01bc13cfd7e2966b1392438
mahesh jamdade <maheshmn121@gmail.com>
2026-08-31 22:20:04 -0400

    skip unrestricted javascript on web

--- lib/ui/webview.dart
+++ lib/ui/webview.dart
@@ -1,47 +1,49 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewPage extends StatefulWidget {
-  static final String routeName = '/privacy';
+  static const String routeName = '/privacy';
+
   final String title;
   final String url;
-  const WebViewPage({Key? key, required this.title, required this.url}) : super(key: key);
+
+  const WebViewPage({
+    Key? key,
+    required this.title,
+    required this.url,
+  }) : super(key: key);
 
   @override
   State<WebViewPage> createState() => _NewPageState();
 }
 
 class _NewPageState extends State<WebViewPage> {
+  late WebViewController controller;
+
   @override
   void initState() {
     super.initState();
-    controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      // ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      // ..setBackgroundColor(const Color(0x00000000))
-      // ..setNavigationDelegate(
-      //   NavigationDelegate(
-      //     onProgress: (int progress) {
-      //       // Update loading bar.
-      //     },
-      //     onPageStarted: (String url) {},
-      //     onPageFinished: (String url) {},
-      //     onWebResourceError: (WebResourceError error) {},
-      //   ),
-      // )
-      ..loadRequest(Uri.parse(widget.url));
+
+    controller = WebViewController();
+
+    if (!kIsWeb) {
+      controller.setJavaScriptMode(JavaScriptMode.unrestricted);
+    }
+
+    controller.loadRequest(Uri.parse(widget.url));
   }
 
-  late WebViewController controller;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: true,
-          title: Text(widget.title),
-        ),
-        body: WebViewWidget(
-          controller: controller,
-        ));
+      appBar: AppBar(
+        automaticallyImplyLeading: true,
+        title: Text(widget.title),
+      ),
+      body: WebViewWidget(
+        controller: controller,
+      ),
+    );
   }
 }
